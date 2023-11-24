8a0610b90681107dd2e27209867b623d1b3d8ebb
kiruthikajanakiraman <kiru2405@gmail.com>
2023-11-24 10:51:32 -0800

    student: view jobs

--- flask-gcp-mysql-demo-main/app/database.py
+++ flask-gcp-mysql-demo-main/app/database.py
@@ -1,12 +1,7 @@
 """Defines all the functions related to the database"""
 from app import db
 
-def fetch_todo(company_id: int) -> dict:
-    """Reads all tasks listed in the todo table
-
-    Returns:
-        A list of dictionaries
-    """
+def fetch_job_postings(company_id: int) -> dict:
 
     conn = db.connect()
     query_results = conn.execute("Select * from Job_Role where company_id={};".format(company_id)).fetchall()
@@ -25,52 +20,45 @@ def fetch_todo(company_id: int) -> dict:
     return roles
 
 
-def update_task_entry(task_id: int, text: str) -> None:
-    """Updates task description based on given `task_id`
+# def update_task_entry(task_id: int, text: str) -> None:
+#     """Updates task description based on given `task_id`
 
-    Args:
-        task_id (int): Targeted task_id
-        text (str): Updated description
+#     Args:
+#         task_id (int): Targeted task_id
+#         text (str): Updated description
 
-    Returns:
-        None
-    """
+#     Returns:
+#         None
+#     """
 
-    conn = db.connect()
-    query = 'Update tasks set task = "{}" where id = {};'.format(text, task_id)
-    conn.execute(query)
-    conn.close()
-
-
-def update_status_entry(task_id: int, text: str) -> None:
-    """Updates task status based on given `task_id`
-
-    Args:
-        task_id (int): Targeted task_id
-        text (str): Updated status
-
-    Returns:
-        None
-    """
-
-    conn = db.connect()
-    query = 'Update tasks set status = "{}" where id = {};'.format(text, task_id)
-    conn.execute(query)
-    conn.close()
+#     conn = db.connect()
+#     query = 'Update tasks set task = "{}" where id = {};'.format(text, task_id)
+#     conn.execute(query)
+#     conn.close()
 
 
-def insert_new_task(id: int, title: str, salary: int, location: str, type: str) ->  int:
-    """Insert new task to todo table.
+# def update_status_entry(task_id: int, text: str) -> None:
+#     """Updates task status based on given `task_id`
 
-    Args:
-        text (str): Task description
+#     Args:
+#         task_id (int): Targeted task_id
+#         text (str): Updated status
 
-    Returns: The task ID for the inserted entry
-    """
+#     Returns:
+#         None
+#     """
 
+#     conn = db.connect()
+#     query = 'Update tasks set status = "{}" where id = {};'.format(text, task_id)
+#     conn.execute(query)
+#     conn.close()
+
+
+def post_job(id: int, title: str, salary: int, location: str, type: str, company_id: int) ->  int:
+    
     conn = db.connect()
     query = 'Insert Into Job_Role VALUES ("{}", "{}", "{}", "{}", "{}", "{}");'.format(
-        id, title, salary, location, type, 1)
+        id, title, salary, location, type, company_id)
     conn.execute(query)
     query_results = conn.execute("Select LAST_INSERT_ID();")
     query_results = [x for x in query_results]
@@ -80,9 +68,63 @@ def insert_new_task(id: int, title: str, salary: int, location: str, type: str) 
     return task_id
 
 
-def remove_task_by_id(job_id: int) -> None:
-    """ remove entries based on task ID """
+def delete_job_posting(job_id: int) -> None:
     conn = db.connect()
     query = 'Delete From Job_Role where job_id={};'.format(job_id)
     conn.execute(query)
     conn.close()
+
+def fetch_job_openings() -> dict:
+    conn = db.connect()
+    query_results = conn.execute("Select * from Job_Role;").fetchall()
+    conn.close()
+    # roles = []
+    # for result in query_results:
+    #     item = {
+    #         "id": result[0],
+    #         "title": result[1],
+    #         "salary": result[2],
+    #         "location": result[3],
+    #         "job_type": result[4]
+    #     }
+    #     roles.append(item)
+
+    # return roles
+    for result in query_results:
+        columns = result.keys()
+    
+    item = [dict(zip(columns, row)) for row in query_results]
+    return item
+
+def fetch_job_openings_by_name(company_name: str) -> dict:
+    conn = db.connect()
+    query = 'SELECT * FROM Job_Role a join Company b on a.company_id = b.company_id where b.company_name like "{}";'.format(company_name)
+    query_results = conn.execute(query).fetchall()
+    conn.close()
+    roles = []
+    # for result in query_results:
+    #     item = {
+    #         "id": result[0],
+    #         "title": result[1],
+    #         "salary": result[2],
+    #         "location": result[3],
+    #         "job_type": result[4]
+    #     }
+    #     roles.append(item)
+
+    for result in query_results:
+        columns = result.keys()
+    
+    item = [dict(zip(columns, row)) for row in query_results]
+    return item
+
+def fetch_jobs_applied(student_id: int) -> dict:
+    conn = db.connect()
+    query = "select c.*, d.company_name from Student a join Applies b on a.student_id = b.student_id join Job_Role c on b.job_id = c.job_id join Company d on c.company_id = d.company_id where a.student_id={};".format(student_id)
+    query_results = conn.execute(query).fetchall()
+    conn.close()
+    for result in query_results:
+        columns = result.keys()
+    
+    item = [dict(zip(columns, row)) for row in query_results]
+    return item
--- flask-gcp-mysql-demo-main/app/routes.py
+++ flask-gcp-mysql-demo-main/app/routes.py
@@ -3,12 +3,12 @@ from flask import render_template, request, jsonify
 from app import app
 from app import database as db_helper
 
-@app.route("/delete/<int:job_id>", methods=['POST'])
-def delete(job_id):
+@app.route("/company/delete/<int:job_id>", methods=['POST'])
+def delete_job_posting(job_id):
     """ recieved post requests for entry delete """
 
     try:
-        db_helper.remove_task_by_id(job_id)
+        db_helper.delete_job_posting(job_id)
         result = {'success': True, 'response': 'Removed job_role'}
     except:
         result = {'success': False, 'response': 'Something went wrong'}
@@ -16,39 +16,58 @@ def delete(job_id):
     return jsonify(result)
 
 
-@app.route("/edit/<int:task_id>", methods=['POST'])
-def update(task_id):
-    """ recieved post requests for entry updates """
+# @app.route("/edit/<int:task_id>", methods=['POST'])
+# def update(task_id):
+#     """ recieved post requests for entry updates """
 
-    data = request.get_json()
+#     data = request.get_json()
 
-    try:
-        if "status" in data:
-            db_helper.update_status_entry(task_id, data["status"])
-            result = {'success': True, 'response': 'Status Updated'}
-        elif "description" in data:
-            db_helper.update_task_entry(task_id, data["description"])
-            result = {'success': True, 'response': 'Task Updated'}
-        else:
-            result = {'success': True, 'response': 'Nothing Updated'}
-    except:
-        result = {'success': False, 'response': 'Something went wrong'}
+#     try:
+#         if "status" in data:
+#             db_helper.update_status_entry(task_id, data["status"])
+#             result = {'success': True, 'response': 'Status Updated'}
+#         elif "description" in data:
+#             db_helper.update_task_entry(task_id, data["description"])
+#             result = {'success': True, 'response': 'Task Updated'}
+#         else:
+#             result = {'success': True, 'response': 'Nothing Updated'}
+#     except:
+#         result = {'success': False, 'response': 'Something went wrong'}
 
-    return jsonify(result)
+#     return jsonify(result)
 
 
-@app.route("/create", methods=['POST'])
-def create():
+@app.route("/company/create", methods=['POST'])
+def post_job():
     """ recieves post requests to add new task """
     data = request.get_json()
-    db_helper.insert_new_task(11112, data['description'], 123456, 'San Francisco', 'Full Time')
+    db_helper.post_job(data['job_id'], data['job_title'], data['salary'], data['location'], data['job_type'], data['company_id'])
     result = {'success': True, 'response': 'Done'}
     return jsonify(result)
 
 
-@app.route("/")
+@app.route("/company/postings")
 def homepage():
     """ returns rendered homepage """
     data = request.get_json()
-    items = db_helper.fetch_todo(data["company_id"])
+    items = db_helper.fetch_job_postings(data["company_id"])
     return render_template("index.html", items=items)
+
+@app.route("/student/job_openings")
+def fetch_job_openings():
+    """ returns available job openings """
+    jobs = db_helper.fetch_job_openings()
+    return jobs
+
+@app.route("/student/job_openings_by_name")
+def fetch_job_openings_by_name():
+    """ returns available job openings """
+    data = request.get_json()
+    jobs = db_helper.fetch_job_openings_by_name(data["company_name"])
+    return jobs
+
+@app.route("/student/applied")
+def fetch_jobs_applied():
+    data = request.get_json()
+    jobs = db_helper.fetch_jobs_applied(data["student_id"])
+    return jobs
